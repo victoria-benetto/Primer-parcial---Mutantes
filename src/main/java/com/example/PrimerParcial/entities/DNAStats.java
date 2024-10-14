4ccc460782e9184e0ba6fa92b3c0c732eb31a3a9
Victoria Benetto <victoriabenetto@gmail.com>
2024-10-14 12:42:50 -0300

    first commit

--- src/main/java/com/example/PrimerParcial/entities/DNAStats.java
+++ src/main/java/com/example/PrimerParcial/entities/DNAStats.java
@@ -4,7 +4,6 @@ import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
-@Table(name = "stats")
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
@@ -12,13 +11,10 @@ import lombok.*;
 @Builder
 public class DNAStats extends Base{
 
-    @Column(name = "mutantCount")
     private long mutantCount;
 
-    @Column(name = "nonMutantCount")
     private long nonMutantCount;
 
-    @Column(name = "mutantRatio")
     private double mutantRatio;
 
 }
